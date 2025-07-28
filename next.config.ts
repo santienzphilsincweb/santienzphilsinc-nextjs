ef9f390e5dfbcb44fc242656dadc7b52ca85a47a
dondycles <johnrod.dondoyano@gmail.com>
2025-07-29 02:04:47 +0800

    config

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "ntcnmlaznccmsdtuqbru.supabase.co",
+        hostname: "olcunvgxuwnxggximads.supabase.co",
       },
       {
         protocol: "https",
